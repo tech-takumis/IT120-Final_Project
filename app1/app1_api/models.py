321fbad04b4015362a0e754758cbd9829d172e4f
ayingleoneilivan <ayingleoneilivan@gmail.com>
2024-12-07 16:33:00 +0800

    Add Transaction model with fields for account, amount, transaction type, and timestamps

--- app1/app1_api/models.py
+++ app1/app1_api/models.py
@@ -20,3 +20,12 @@ class BankAccount(models.Model):
     
 def __str__(self):
     return f"Account {self.account_number} - {self.bankname}"
+
+class Transaction(models.Model):
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
